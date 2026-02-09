6dd986e7c7bce0547b0b3258046b7b3c2e69ada4
Barış Kandemir <bariskandemir@192.168.1.19>
2026-02-09 23:28:29 +0300

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,16 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Icon } from '@/src/shared/ui'
+import { Tabs } from 'expo-router'
+import React from 'react'
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+const _layout = () => {
+    return (
+        <Tabs screenOptions={{ headerShown: false }}>
+            <Tabs.Screen name="index" options={{ title: "", tabBarIcon: () => <Icon icon="home" /> }} />
+            <Tabs.Screen name="favorite" options={{ title: "", tabBarIcon: () => <Icon icon="heart" /> }} />
+            <Tabs.Screen name="basket" options={{ title: "", tabBarIcon: () => <Icon icon="bag" /> }} />
+            <Tabs.Screen name="notifications" options={{ title: "", tabBarIcon: () => <Icon icon="bell" /> }} />
+        </Tabs>
+    )
 }
+
+export default _layout
---
+++ src/shared/ui/icon/index.tsx
@@ -0,0 +1,27 @@
+import iconSet from '@/assets/selection.json';
+import IcoMoon from "react-icomoon";
+import { Svg, Path } from "react-native-svg";
+
+type IconName = typeof iconSet.icons[number]['icon']['tags'][number];
+
+type TIconProps = {
+    icon: IconName;
+    size?: number;
+    color?: string;
+}
+
+const Icon = ({ icon, size = 24, color = "#000" }: TIconProps) => {
+    return (
+        <IcoMoon
+            native
+            SvgComponent={Svg}
+            PathComponent={Path}
+            iconSet={iconSet}
+            icon={icon}
+            size={size}
+            color={color}
+        />
+    )
+}
+
+export { Icon };
