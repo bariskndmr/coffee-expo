bfd39367082f7f6a770d6e17c06f379f86eaa235
Barış Kandemir <bariskandemir@192.168.1.3>
2026-02-12 22:52:57 +0300

    feat: onboarding enhancemenets

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,7 +1,8 @@
 import { OnboardingSvg } from '@/assets/images';
+import { colors } from '@/src/shared';
 import { LinearGradient } from 'expo-linear-gradient';
 import React from 'react';
-import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, useWindowDimensions } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const OnboardingScreen = () => {
@@ -18,10 +19,23 @@ const OnboardingScreen = () => {
                         style={[StyleSheet.absoluteFill, { top: imageHeight * 0.9, height: imageHeight * 0.1 }]}
                     />
                 </View>
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={{ color: "#fff" }}>Onboarding</Text>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', marginHorizontal: 24, gap: 12 }}>
+                    <Text style={{ color: colors.surface.white.default, lineHeight: 50, fontFamily: "sora-semibold", fontSize: 32, textAlign: 'center' }}>Fall in Love with Coffee in Blissful Delight!</Text>
+                    <Text style={{ color: colors.grey.lighter, fontFamily: "sora-regular", fontSize: 14, textAlign: 'center' }}>Welcome to our cozy coffee corner, where every cup is a delightful for you.</Text>
+
+                    <TouchableOpacity style={{
+                        backgroundColor: colors.brown.normal.default,
+                        paddingVertical: 16,
+                        width: '100%',
+                        borderRadius: 16,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        marginVertical: 24,
+                    }}>
+                        <Text style={{ color: colors.surface.white.default, fontFamily: "sora-semibold", fontSize: 16 }}>Get Started</Text>
+                    </TouchableOpacity>
                 </View>
-            </SafeAreaView>
+            </SafeAreaView >
         </>
     )
 }
